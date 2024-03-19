3e267588ec21d78c271957844dda393cffa00934
Bruno Ricardo <brunomatheus2607@gmail.com>
2024-03-18 22:22:53 -0300

    arrumando e adicionando loop na template

--- src/template.cpp
+++ src/template.cpp
@@ -11,8 +11,6 @@ typedef long double ld;
 const ll maxn = 2e5 + 10;
 const ll inf = LLONG_MAX;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-
 #define pb push_back
 #define ppb pop_back 
 #define ff first
@@ -20,9 +18,18 @@ template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, 
 #define sza(x) ((int)(x).size())
 #define all(a) (a).begin(), (a).end()
 
+#define FF(i, n) for(int i = 0; i < (n); i++)
+#define FF1(i, n) for(int i = 1; i <= (n); i++)
+#define FFZ(i, v) FF(i, sza(v))
+#define F(n) FF(i, n)
+#define F1(n) FF1(i, n)
+#define FZ(v) FFZ(i, v)
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << "(" << p.ff << ", " << p.ss << ")"; }
+
 #ifdef LOCAL
 void dbg_out() { cerr << endl; }
-template<typename H, typename... T> void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+template<typename H, typename... T> void dbg_out(H h, T... t) { cerr << " " << h; dbg_out(t...); }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
 #define dbg(...)
@@ -33,8 +40,8 @@ ll a[maxn];
 
 void solve() {
     cin >> n;
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
+    F(n) {
+    	cin >> a[i];
     }
 }
 
@@ -45,5 +52,5 @@ int main() {
     int t;
     cin >> t;
 
-    for(int i = 1; i <= t; i++) solve();
+    F1(t) solve();
 }
