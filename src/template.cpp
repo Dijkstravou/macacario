e38081e1148a659a00d44bfaadf17b5afa51f125
brun0-matheus <brunomatheus2607@gmail.com>
2024-08-20 14:44:07 -0300

    mudancas template

--- src/template.cpp
+++ src/template.cpp
@@ -7,6 +7,8 @@ using namespace std;
 typedef long long ll;
 typedef pair<ll, ll> pii;
 typedef long double ld;
+typedef vector<ll> vi;
+typedef vector<pii> vpi;
 
 const ll maxn = 2e5 + 10;
 const ll inf = LLONG_MAX;
@@ -17,15 +19,23 @@ const ll inf = LLONG_MAX;
 #define ss second 
 #define sza(x) ((int)(x).size())
 #define all(a) (a).begin(), (a).end()
+#define smax(s, v) s = max(s, (v))
+#define smin(s, v) s = min(s, (v))
 
 #define FF(i, n) for(int i = 0; i < (n); i++)
 #define FF1(i, n) for(int i = 1; i <= (n); i++)
 #define FFZ(i, v) FF(i, sza(v))
+#define FFS(i, s, n) for(int i = s; i <= (n); i++)
+#define FFD(i, n) for(int i = (n)-1; i >= 0; i--)
+
 #define F(n) FF(i, n)
 #define F1(n) FF1(i, n)
 #define FZ(v) FFZ(i, v)
+#define FS(s, n) FFS(i, s, n)
+#define FD(n) FFD(i, n)
 
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << "(" << p.ff << ", " << p.ss << ")"; }
+template<typename A> ostream& operator<<(ostream& os, const vector<A>& v) { os << "["; for(const auto& x: v) os << x << ", "; return os << "]"; }
 
 #ifdef LOCAL
 void dbg_out() { cerr << endl; }
@@ -47,7 +57,7 @@ void solve() {
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
 
     int t;
     cin >> t;
